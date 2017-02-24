ff2e6ec3a0fec636865f0f84dcddc1862d0e9371
gino serpa <gino.serpa@gmail.com>
2017-02-23 22:07:22 -0500

    Initial doodling with rays and planes. Need to change planes to detectors. A bit confused about orienting them

--- WOM.py
+++ WOM.py
@@ -1,1 +1,41 @@
-print 1/7.
+
+'''
+
+Things I know I will need
+
+* A function to calculate the interception of a ray with a plane
+* A function to calculate interception of a ray with a sphere
+
+I have no class, so no classes
+
+'''
+
+import scipy as sp
+
+def get_ray_plane_intercept(ray, plane):
+    n = plane['normal']
+    d = ray['direction']
+    denominator = sp.inner(d, n)
+    if denominator==0:
+        return None
+    else:
+        P_0p = plane['point']
+        P_0r = ray['origin']
+        numerator = sp.inner(P_0p-P_0r, n)
+        t = numerator/denominator
+        point = P_0r + t*d
+        return point
+    return
+
+
+# Define a ray
+ray = {'origin':sp.array([0.,0.,0.]), 'direction':sp.array([0., sp.sqrt(2.)/2, sp.sqrt(2.)/2])}
+print 'ray = ', ray
+
+# Define a plane
+plane = {'point':sp.array([0.,1.,0.]), 'normal':sp.array([0.,1.,0.])}
+print 'plane = ', plane
+
+# Find the interception
+point = get_ray_plane_intercept(ray, plane)
+print point
