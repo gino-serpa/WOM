7152864e2b0d02dc953bd89757d2c422de9cc283
gino serpa <gino.serpa@gmail.com>
2017-02-24 11:20:31 -0500

    CLeaned up notation. I think I need classes but I want to get some display out

--- WOM.py
+++ WOM.py
@@ -6,11 +6,54 @@ Things I know I will need
 * A function to calculate the interception of a ray with a plane
 * A function to calculate interception of a ray with a sphere
 
-I have no class, so no classes
+Maybe I need classes
 
 '''
 
 import scipy as sp
+
+def define_detector(d):
+    detector = {'point':sp.array([0., d,0.]), \
+             'normal':sp.array([0.,1.,0.]),\
+             'width':  10.,\
+             'height': 10.,\
+             'width pixels': 100,\
+             'height pixels': 100}
+    return detector
+
+def detector_info(detector):
+    print 'Detector Information'
+    print 'Point :', detector['point']
+    print 'Normal :',detector['normal']
+    print 'Width :',detector['width']
+    print 'Height :',detector['height']
+    print 'Width pixels :',detector['width pixels']
+    print 'Height pixels :',detector['height pixels']
+    print
+    return
+
+def define_source():
+    source = {'origin':sp.array([0., 0., 0.])}
+    return source
+
+def source_info(source):
+    print "Source Information"
+    for key in source.keys():
+        print key,': ',source[key]
+    print
+    return
+
+def generate_ray(source):
+    ray={}
+    ray['direction'] = sp.array([1.,2.,1.])
+    ray['origin']    = sp.array([0.,0.,0.])
+    return ray
+
+def ray_info(ray):
+    print 'Ray information'
+    for key in ray.keys():
+        print key,': ',ray[key]
+    return
 
 def get_ray_detector_intercept(ray, plane):
     n = plane['normal']
@@ -27,28 +70,35 @@ def get_ray_detector_intercept(ray, plane):
         return point
     return
 
+
 '''----------------------------------------------------
 
                     Main
-
+Gino Serpa
 ---------------------------------------------------- '''
 
-'''# Define a ray
-ray = {'origin':sp.array([0.,0.,0.]), \
-       'direction':sp.array([0., sp.sqrt(2.)/2, sp.sqrt(2.)/2])}
-print 'ray = ', ray
-'''
+
 
 # Define a detector
-detector = {'point':sp.array([0.,1.,0.]), \
-         'normal':sp.array([0.,1.,0.])}
-print 'detector = ', detector
+detector_1 = define_detector(2)
+detector_info(detector_1)
 
-# Find the interception
-point_source = {'origin':sp.array([0., 0., 0.])}
 
+# Define a source
+point_source = define_source()
+source_info(point_source)
+
+# Ray Trace
 iterations = 100
 i=0
 while i < iterations:
-    ray = generate_ray()
-    point = get_ray_detector_intercept(ray, detector)
+    # Generate a ray from the source
+    ray = generate_ray(point_source)
+    ray_info(ray)
+
+    # Find the interception with the detector
+    point = get_ray_detector_intercept(ray, detector_1)
+    # Accumulate energy in the detector
+    i+=1
+
+# Display the detector
