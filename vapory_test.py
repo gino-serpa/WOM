172c3b7f71a7ffa2483b7d019e80b7f568111872
gino serpa <gino.serpa@gmail.com>
2017-03-05 11:43:39 -0500

    A bit more order in the rendering. I can almost see a real lens though

--- vapory_test.py
+++ vapory_test.py
@@ -1,33 +1,58 @@
-
+import scipy as sp
 
 from vapory import *
 
 from WOMlenses import *
 
+RED    = [ 1, 0, 0]
+GREEN  = [ 0, 1, 0]
+BLUE   = [ 0, 0, 1]
+ORIGIN = [ 0, 0, 0]
+WHITE =  [ 1, 1, 1]
+X = sp.array([ 1, 0, 0])
+Y = sp.array([ 0, 1, 0])
+Z = sp.array([ 0, 0, 1])
+
+def get_pov_lens(lens):
+    print lens
+    radius = lens['lens_charateristics']['Radius of Curvature']
+    thickness = lens['lens_charateristics']['Center Thickness']
+    sphere1 = Sphere(radius*Y,
+                      radius)
+    sphere2 = Sphere((-radius+thickness)*Y,
+                      radius)
+    pov_lens = Intersection(sphere1,sphere2,
+                            Texture(Pigment('color', BLUE)))
+    return pov_lens
+
+'''
+2
+'''
+
 # Set the lights
-light1 = LightSource( [30, -30, 30], \
-                     'color', [1, 1, 1] )
-light2 = LightSource( [30,  30, 30], \
-                     'color', [1, 1, 1] )
-light3 = LightSource( [30,  -30, -30], \
-                    'color', [1, 1, 1] )
-light4 = LightSource( [30,  30, -30], \
-                      'color', [1, 1, 1] )
+light1 = LightSource( [30, -30, 30],
+                     'color', WHITE )
+light2 = LightSource( [30,  30, 30],
+                     'color', WHITE )
+light3 = LightSource( [30,  -30, -30],
+                     'color', WHITE )
+light4 = LightSource( [30,  30, -30],
+                     'color', WHITE )
 objects = [light1, light2, light3, light4]
 
 # Set the camera
-camera = Camera( 'location', [48, 12, 12],\
-                 'look_at', [0, 0, 0] )
+camera = Camera( 'location', 41*(X),
+                 'look_at', ORIGIN )
 
 # Draw the three axis
 L = 40.
 r_axis = 0.06
-x_axis = Cylinder([ 0, 0, 0], [0,0,L], r_axis,\
-                    Texture( Pigment( 'color', [1., 0., 0.])))
-y_axis = Cylinder([ 0, 0, 0], [ 0, L, 0], r_axis,\
-                    Texture( Pigment( 'color', [0., 1., 0.])))
-z_axis = Cylinder([ 0, 0, 0], [ L, 0, 0], r_axis,\
-                    Texture( Pigment( 'color', [0, 0., 1.])))
+x_axis = Cylinder(ORIGIN, L*X, r_axis,
+                    Texture( Pigment( 'color', RED)))
+y_axis = Cylinder(ORIGIN, L*Y, r_axis,\
+                    Texture( Pigment( 'color', GREEN)))
+z_axis = Cylinder(ORIGIN, L*Z, r_axis,\
+                    Texture( Pigment( 'color', BLUE)))
 objects.append(x_axis)
 objects.append(y_axis)
 objects.append(z_axis)
@@ -35,10 +60,10 @@ objects.append(z_axis)
 # Set a bi convex lens
     # Get the lens
 lens1 = choose_lens('Newport KBX043', 10.)
-sphere1 = Sphere([0,0,0],\
-                lens1['lens_charateristics']['Radius of Curvature'],\
-                Texture(Pigment('color',[0,0,1])))
-objects.append(sphere1)
+
+pov_lens1 = get_pov_lens(lens1)
+objects.append(pov_lens1)
+
 '''
 d = 2.6
 sphere1 = Sphere( [0, d, 0], 3)
